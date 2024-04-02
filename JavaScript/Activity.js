01e3cc8d912fd6f7487653c1e91bf11dabde2767
Jayashree0309 <139547551+Jayashree0309@users.noreply.github.com>
2024-04-02 06:30:51 +0530

    Update Activity.js

--- JavaScript/Activity.js
+++ JavaScript/Activity.js
@@ -1,28 +1,11 @@
 //Array 
-const names=["Janani","Jayapriya","Jolin"];
+const Studentdetails=["StudentId","Name","Standard","age","gender"];
 console.log(names);
 console.log(names.pop());
-console.log(names.push("Jayashree"));
+console.log(names.push("Marks"));
 console.log(names);
 let arrlen=names.length;
 console.log(arrlen);
-
-// Map functions
-console.log("-----------Map Function---------------");
-const colours = ["red", "yellow", "orange", "pink", "green"];
-
-// convert a string to uppercase
-function capitalize(str) {
-  return str.toUpperCase();
-}
-const capitalizedColours = colours.map(capitalize);
-console.log("Original Colours: ", colours);
-console.log("Capitalized Colurs: ", capitalizedColours);
-console.log("--------------sortfunction---------------");
-console.log(capitalizedColours.sort());
-console.log("--------------Reversefunction---------------");
-console.log(capitalizedColours.reverse());
-
 
 console.log("-----------find function------------");
 const students = [
@@ -45,8 +28,8 @@ console.log("-----------findIndex function------------");
 function isKathir(student) {
     return student.name === "Kathir";
 }
-const KathirIndex = students.findIndex(isKathir);
-console.log("Index of First Student Named Kathir: ", KathirIndex);
+const Index = students.findIndex(isKathir);
+console.log("Index of First Student Named Kathir: ", Index);
 console.log("\n");
 
 console.log("-----------setTimeout function------------");
